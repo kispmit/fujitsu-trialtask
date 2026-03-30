d5a9ed2941df4438d0bb6311fc1dbafa3822d966
tim <timmu.ingo@gmail.com>
2026-03-30 16:24:22 +0300

    adding springboot

---
+++ src/main/java/com/tim/deliveryfee/DeliveryfeeApplication.java
@@ -0,0 +1,13 @@
+package com.tim.deliveryfee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryfeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryfeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tim/deliveryfee/DeliveryfeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim.deliveryfee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryfeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
